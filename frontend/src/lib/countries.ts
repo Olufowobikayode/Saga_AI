4af11ec6c99e7bbaf593dd87f7329f8d187510dc
Olufowobikayode <152504854+Olufowobikayode@users.noreply.github.com>
2025-07-23 14:22:26 +0100

    Create lib/countries.ts

---
+++ frontend/src/lib/countries.ts
@@ -0,0 +1,23 @@
+// --- START OF FILE src/lib/countries.ts ---
+// This file provides a curated list of countries for the selector.
+// It can be easily expanded or modified in the future.
+
+export const countries = [
+  { code: 'global', name: 'Global' },
+  { code: 'us', name: 'United States' },
+  { code: 'gb', name: 'United Kingdom' },
+  { code: 'ca', name: 'Canada' },
+  { code: 'au', name: 'Australia' },
+  { code: 'de', name: 'Germany' },
+  { code: 'fr', name: 'France' },
+  { code: 'es', name: 'Spain' },
+  { code: 'it', name: 'Italy' },
+  { code: 'jp', name: 'Japan' },
+  { code: 'in', name: 'India' },
+  { code: 'br', name: 'Brazil' },
+  { code: 'mx', name: 'Mexico' },
+  { code: 'cn', name: 'China' },
+  { code: 'za', name: 'South Africa' },
+  // Add more countries as needed
+];
+// --- END OF FILE src/lib/countries.ts ---
