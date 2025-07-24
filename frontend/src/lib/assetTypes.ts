a5a9b1baea25e64c3ac88c4b76c58376e170c59a
Olufowobikayode <152504854+Olufowobikayode@users.noreply.github.com>
2025-07-24 08:02:19 +0100

    Create assetTypes.ts

---
+++ frontend/src/lib/assetTypes.ts
@@ -0,0 +1,11 @@
+// --- START OF FILE src/lib/assetTypes.ts ---
+export const assetTypes = [
+  "Product", "Service", "Community", "Store", "Affiliate", 
+  "Ads", "Funnel", "Landing Page", "Blog", "Website", "Other"
+];
+
+export const linkTypes = [
+  "Affiliate Link", "Service Link", "Product Link", "Group Link", 
+  "Channel Link", "Store Link", "Funnel Link", "Website Link", "Other"
+];
+// --- END OF FILE src/lib/assetTypes.ts ---
